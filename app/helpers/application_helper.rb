ce1c4b7dd7c400b715dd2fde6975e974afb66674
robmckinnon <rob@movingflow>
2016-04-21 17:34:08 +0100

    Add `radio_button_fieldset` stacked/inline radio button examples
    Add example for `radio_button_fieldset` producing inline radio buttons.
    
    Add example for `radio_button_fieldset` producing stacked radio buttons.
    
    Add examples for setting legend, hint, and label text via locales file.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def format_html_example html
+    "\n" + html.
+      gsub('><', ">\n  <").
+      gsub('  </', '</').
+      gsub(%r[</span>\n</(label|legend)>], '</span></\1>').
+      gsub("\n<label", "\n  <label").
+      gsub(%r[<input(.+)</label>]) { "  <input#{$1}\n  </label>" }
+  end
 end
