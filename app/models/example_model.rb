6b489785aedb2141a70bc4ab059317ab3bf73a2f
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2017-03-15 17:03:37 +0000

    Remove old version 0

--- app/models/example_model.rb
+++
@@ -1,5 +0,0 @@
-class ExampleModel
-  include ActiveModel::Model
-
-  attr_accessor :name, :address, :status
-end
--- app/models/waste_transport.rb
+++
@@ -1,8 +0,0 @@
-class WasteTransport
-  include ActiveModel::Model
-
-  attr_accessor :animal_carcasses
-  attr_accessor :mines_quarries
-  attr_accessor :farm_agricultural
-
-end
